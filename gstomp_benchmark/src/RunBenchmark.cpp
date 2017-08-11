fe7bf8afe7920911234453f38c29d2ae496b4575
ipa-mjp <mayank.jitendrakumar.patel@ipa.fraunhofer.de>
2017-08-11 15:48:08 +0200

    only benchmarking or test of stomp planner

--- gstomp_benchmark/src/RunBenchmark.cpp
+++ gstomp_benchmark/src/RunBenchmark.cpp
@@ -1,5 +1,3 @@
-
-
 #include <kdl_parser/kdl_parser.hpp>
 #include <ros/ros.h>
 #include <ros/package.h>
@@ -12,40 +10,12 @@
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit/collision_plugin_loader/collision_plugin_loader.h>
 #include <fstream>
-
-#include <moveit/planning_interface/planning_interface.h>
-#include <moveit/planning_scene/planning_scene.h>
-#include <pluginlib/class_loader.h>
-#include <pluginlib/pluginlib_exceptions.h>
 #include <stomp_moveit/stomp_planner.h>
 
 using namespace ros;
 using namespace Eigen;
 using namespace moveit::core;
 using namespace std;
-
-bool getConfigData(ros::NodeHandle &nh, std::map<std::string, XmlRpc::XmlRpcValue> &config, std::string param)
-{
-  // Create a stomp planner for each group
-  XmlRpc::XmlRpcValue stomp_config;
-  if(!nh.getParam(param, stomp_config))
-  {
-    ROS_ERROR("The 'stomp' configuration parameter was not found");
-    return false;
-  }
-
-  // each element under 'stomp' should be a group name
-  std::string group_name;
-  for(XmlRpc::XmlRpcValue::iterator v = stomp_config.begin(); v != stomp_config.end(); v++)
-  {
-     group_name = static_cast<std::string>(v->second["group_name"]);
-     config.insert(std::make_pair(group_name, v->second));
-  }
-
-  ROS_WARN("Reading data successfully");
-    return true;
-
-}
 
 template<typename T = string>
 void printVector(vector<T> vec)
@@ -61,6 +31,50 @@ void printValue(T value)
 }
 
 
+void collectData(const planning_interface::MotionPlanDetailedResponse& res)
+{
+	double length_traj = 0.0, smoothness;
+
+	for (size_t i = 0; i < res.trajectory_.size(); ++i)
+	{
+		const robot_trajectory::RobotTrajectory& p = *res.trajectory_[i];
+
+		// compute path length
+		for (std::size_t k = 1; k < p.getWayPointCount(); ++k)
+			length_traj += p.getWayPoint(k - 1).distance(p.getWayPoint(k));
+
+		//compute smoothness //this is gives interpolation smoothness not path plan smoothness
+		if (p.getWayPointCount() > 2)
+		{
+			double a = p.getWayPoint(0).distance(p.getWayPoint(1));
+
+		    for (std::size_t k = 2; k < p.getWayPointCount(); ++k)
+		    {
+		    	double b = p.getWayPoint(k-1).distance(p.getWayPoint(k));
+		    	double cdist = p.getWayPoint(k-2).distance(p.getWayPoint(k));
+		    	double acosValue = (a * a + b * b - cdist * cdist) / (2.0 * a * b);
+		    	if (acosValue > -1.0 && acosValue < 1.0)
+		    	{
+		    	 // the smoothness is actually the outside angle of the one we compute
+		    	 double angle = (3.1415 - acos(acosValue));
+
+		    	 // and we normalize by the length of the segments
+		    	  double u = 2.0 * angle;  /// (a + b);
+		    	  smoothness += u * u;
+		    	 }
+		    	a = b;
+		      }
+		   smoothness /= double(p.getWayPointCount());
+		  }
+
+		ROS_WARN_STREAM("lenght_traj: " <<length_traj);
+		ROS_WARN_STREAM("smoothness: " <<smoothness);
+		ROS_WARN_STREAM("time: " <<res.processing_time_[i]);
+	}
+}
+
+
+
 int main (int argc, char *argv[])
 {
 	ros::init(argc, argv, "gstomp_run_benchmark");
@@ -70,9 +84,6 @@ int main (int argc, char *argv[])
 	robot_model_loader::RobotModelLoaderPtr loader;
 	robot_model::RobotModelPtr robot_model;
 	string urdf_file_path, srdf_file_path;
-
-	std::shared_ptr<pluginlib::ClassLoader<planning_interface::PlannerManager> > planner_plugin_loader;
-	planning_interface::PlannerManagerPtr planner_instance;
 
 	urdf_file_path = package::getPath("ipa_hardware_config") + "/robots/raw3-1/urdf/raw3-1.urdf";
 	srdf_file_path = package::getPath("ipa_moveit_config") + "/robots/raw3-1/moveit/config/raw3-1.srdf";
@@ -93,50 +104,15 @@ int main (int argc, char *argv[])
 	  return false;
 	}
 
-	//---------------------------------------- plugin ---------------------------
-
-	std::string planner_plugin_name = "stomp_moveit/StompPlannerManager";
-	try
-	{
-	  planner_plugin_loader.reset(new pluginlib::ClassLoader<planning_interface::PlannerManager>("moveit_core", "planning_interface::PlannerManager"));
-	}
-	catch(pluginlib::PluginlibException& ex)
-	{
-	  ROS_FATAL_STREAM("Exception while creating planning plugin loader " << ex.what());
-	}
-	try
-	{
-	  planner_instance.reset(planner_plugin_loader->createUnmanagedInstance(planner_plugin_name));
-	  if (!planner_instance->initialize(robot_model, nh.getNamespace()))
-	    ROS_FATAL_STREAM("Could not initialize planner instance");
-	  ROS_INFO_STREAM("Using planning interface '" << planner_instance->getDescription() << "'");
-	}
-	catch(pluginlib::PluginlibException& ex)
-	{
-	  const std::vector<std::string> &classes = planner_plugin_loader->getDeclaredClasses();
-	  std::stringstream ss;
-	  for (std::size_t i = 0 ; i < classes.size() ; ++i)
-	    ss << classes[i] << " ";
-	  ROS_ERROR_STREAM("Exception while loading planner '" << planner_plugin_name << "': " << ex.what() << std::endl
-	                   << "Available plugins: " << ss.str());
-	}
-
-	ros::WallDuration sleep_time(5.0);
-	sleep_time.sleep();
-
-	//--------------------------------------------------------------------
-	//vector<std::string> jnames = robot_model->getJointModelNames();
-
-	//printVector<std::string>(jnames);
-
-	getConfigData(nh, config, "stomp");
-
-	//stomp_moveit::StompPlanner::getConfigData(nh, config);
+	stomp_moveit::StompPlanner::getConfigData(nh, config);
 
 	planning_scene::PlanningScenePtr planning_scene(new planning_scene::PlanningScene(robot_model));
 	planning_interface::MotionPlanRequest req;
-	planning_interface::MotionPlanResponse res;
+	//planning_interface::MotionPlanResponse res;
+	planning_interface::MotionPlanDetailedResponse res;
 	std::string group_name = "arm";
+
+	stomp_moveit::StompPlanner stomp(group_name, config[group_name], robot_model);	//main line for stomp
 
 	req.allowed_planning_time = 10;
 	req.group_name =group_name;
@@ -164,7 +140,7 @@ int main (int argc, char *argv[])
 	jgoal.insert(make_pair("arm_wrist_2_joint", -1.81));
 	jgoal.insert(make_pair("arm_wrist_3_joint", 0.00));
 
-	start.setVariablePositions(jgoal);
+	goal.setVariablePositions(jgoal);
 
 	vector<double> dist(7);
 	  dist[0] = 0.05;
@@ -179,11 +155,9 @@ int main (int argc, char *argv[])
 	  t1 = ros::Time::now();
 	  const robot_state::JointModelGroup *jmg = goal.getJointModelGroup(group_name);
 
-	  planning_interface::PlanningContextPtr context = planner_instance->getPlanningContext(planning_scene, req, res.error_code_);
-
 	  ROS_WARN("Hello");
 
-	  int test_runs = 1;
+	  int test_runs = 10;
 
 	  for (int i = 0; i < test_runs; i++)
 	    {
@@ -194,22 +168,18 @@ int main (int argc, char *argv[])
 	        req.goal_constraints.resize(1);
 	        req.goal_constraints[0] = kinematic_constraints::constructGoalConstraints(new_goal, jmg);
 	      }
-		  ROS_WARN("Hello");
-		  context->clear();
-	      context->setPlanningScene(planning_scene);
-	      context->setMotionPlanRequest(req);
-		  ROS_WARN("Hello");
-	      if (context->solve(res))
-	    	  ROS_ERROR_STREAM("STOMP Solver failed:" << res.error_code_);
 
-	      if(res.error_code_.val != res.error_code_.SUCCESS)
-	      {
-	      	ROS_ERROR("Could not compute plan successfully");
-	      	return 0;
-	      }
+	      stomp.clear();
+	      stomp.setPlanningScene(planning_scene);
+	      stomp.setMotionPlanRequest(req);
 
+	      if (!stomp.solve(res))
+	            ROS_ERROR_STREAM("STOMP Solver failed:" << res.error_code_);
+
+	      collectData(res);
 	    }
-
+	  t2 = ros::Time::now();
+	  ROS_ERROR("Average time spent calculating trajectory: %4.10f seconds", (t2-t1).toSec()/test_runs);
 	ROS_WARN("done...");
 	ros::spin();
 
